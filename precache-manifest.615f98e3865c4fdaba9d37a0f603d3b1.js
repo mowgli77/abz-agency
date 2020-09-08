15be4c86a248c16ba2fd6fbc6fc575c08c3f4ed1
mowgli77 <bartend.it13@gmail.com>
2020-09-08 16:26:59 +0300

    Updates

---
+++ precache-manifest.615f98e3865c4fdaba9d37a0f603d3b1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "730d93d057aae9e4fd3d5bca68eddcec",
+    "url": "/abz-agency/index.html"
+  },
+  {
+    "revision": "7e2baedb542f6e9c3d70",
+    "url": "/abz-agency/static/css/main.7732fb94.chunk.css"
+  },
+  {
+    "revision": "d7e32e10fbd675a4e4a5",
+    "url": "/abz-agency/static/js/2.534e1c59.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/abz-agency/static/js/2.534e1c59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e2baedb542f6e9c3d70",
+    "url": "/abz-agency/static/js/main.d0e9eb5e.chunk.js"
+  },
+  {
+    "revision": "d913e2b8bb3d21e7c600",
+    "url": "/abz-agency/static/js/runtime-main.949ba4b2.js"
+  },
+  {
+    "revision": "f5443d74025f2a6069e0ca0142c08a05",
+    "url": "/abz-agency/static/media/banner-photo.f5443d74.jpg"
+  },
+  {
+    "revision": "146e29265bcc680129e263a9478dca00",
+    "url": "/abz-agency/static/media/logo.146e2926.svg"
+  },
+  {
+    "revision": "3cdf78971c869b16b95152be7b6922e5",
+    "url": "/abz-agency/static/media/man-laptop-v1.3cdf7897.svg"
+  },
+  {
+    "revision": "6f6bef288009ad20ca333f0cd61608c6",
+    "url": "/abz-agency/static/media/menu-icon.6f6bef28.svg"
+  }
+]);
